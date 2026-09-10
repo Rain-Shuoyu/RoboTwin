63f342784c79feff127115edee5b2809f28d5832
ShuoyuChen <chenshy357@mail2.sysu.edu.cn>
2026-09-10 15:31:15 +0800

    fix: keep cafe collection tray static

--- envs/cafe_table_reset.py
+++ envs/cafe_table_reset.py
@@ -48,11 +48,11 @@ class cafe_table_reset(Base_Task):
                 result.set_mass(mass)
             return result
 
-        self.cup = actor("021_cup", [-0.28, -0.25, 0.741], quat=[0.5, 0.5, 0.5, 0.5])
-        self.mug = actor("039_mug", [0.00, -0.25, 0.741], quat=[0.5, 0.5, 0.5, 0.5])
-        self.plate = actor("003_plate", [0.28, -0.25, 0.741], quat=[0.5, 0.5, 0.5, 0.5])
+        self.cup = actor("021_cup", [-0.30, -0.18, 0.741], quat=[0.5, 0.5, 0.5, 0.5])
+        self.mug = actor("039_mug", [0.00, -0.18, 0.741], quat=[0.5, 0.5, 0.5, 0.5])
+        self.plate = actor("003_plate", [0.30, -0.18, 0.741], quat=[0.5, 0.5, 0.5, 0.5])
         self.tissue_box = actor("023_tissue-box", [-0.42, 0.18, 0.741], is_static=True, quat=[0.5, 0.5, 0.5, 0.5])
-        self.tray = actor("008_tray", [0.16, 0.06, 0.741], mass=0.12, quat=[0.706527, 0.706483, -0.0291356, -0.0291767])
+        self.tray = actor("008_tray", [0.00, 0.12, 0.741], is_static=True, mass=0.12, quat=[0.706527, 0.706483, -0.0291356, -0.0291767])
         self.table_bin = actor("063_tabletrashbin", [0.38, 0.16, 0.741], is_static=True, quat=[0.5, 0.5, 0.5, 0.5])
         self.cleaner = actor("096_cleaner", [0.26, 0.20, 0.741], is_static=True)
         self.roll_paper = actor("028_roll-paper", [0.00, 0.20, 0.741], model_id=1, is_static=True, quat=[0.5, 0.5, 0.5, 0.5])
@@ -83,8 +83,6 @@ class cafe_table_reset(Base_Task):
         self._place_on_tray(self.mug, "right", [0.06, 0.00])
         self._place_on_tray(self.plate, "left", [0.00, 0.06])
 
-        self._place_in_region(self.tray, "right", [0.31, -0.02], z=0.80)
-
         self.move(self.grasp_actor(self.bell, arm_tag="right", pre_grasp_dis=0.10, grasp_dis=0.10, contact_point_id=0))
         self.move(self.move_by_displacement("right", z=-0.045))
         self.stage_success_tag = self._bell_contact()
@@ -107,14 +105,12 @@ class cafe_table_reset(Base_Task):
 
     def check_success(self):
         tray_center = self.tray.get_pose().p[:2]
-        cleaning_center = self.cleaning_zone.get_pose().p[:2]
-        bin_center = self.table_bin.get_pose().p[:2]
         basket_center = self.breadbasket.get_pose().p[:2]
         supplies_ready = self._in_region(self.tissue_box, [-0.42, 0.18]) and self._in_region(self.cleaner, [0.26, 0.20])
         dishes_cleared = all(self._in_region(item, tray_center, (0.14, 0.12)) for item in (self.cup, self.mug, self.plate))
         return bool(
             dishes_cleared
-            and self._in_region(self.tray, cleaning_center, (0.14, 0.12))
+            and self._in_region(self.tray, tray_center, (0.14, 0.12))
             and self._in_region(self.bread, basket_center, (0.12, 0.12))
             and supplies_ready
             and self.stage_success_tag
